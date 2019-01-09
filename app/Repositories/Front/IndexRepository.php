45f67e170821dc354d6602ab3ecf26cbec329bde
longyun <longyun@longyunMacBook-Pro.local>
2019-01-09 18:07:26 +0800

    cover_pic

--- app/Repositories/Front/IndexRepository.php
+++ app/Repositories/Front/IndexRepository.php
@@ -27,21 +27,21 @@ class IndexRepository {
 //        $info = json_decode(json_encode(config('mitong.company.info')));
 //        $menus = RootMenu::where(['active'=>1])->orderby('order', 'asc')->get();
 
-        $rent_items = RootItem::where(['category'=>11, 'active'=>1])->orderby('id', 'desc')->limit(8)->get();
+        $rent_items = RootItem::where(['category'=>11, 'active'=>1])->orderby('updated_at', 'desc')->limit(8)->get();
         foreach($rent_items as $item)
         {
             $item->custom = json_decode($item->custom);
         }
 
-        $wholesale_items = RootItem::where(['category'=>12, 'active'=>1])->orderby('id', 'desc')->limit(4)->get();
+        $wholesale_items = RootItem::where(['category'=>12, 'active'=>1])->orderby('updated_at', 'desc')->limit(4)->get();
         foreach($wholesale_items as $item)
         {
             $item->custom = json_decode($item->custom);
         }
 
-        $cooperation_items = RootItem::where(['category'=>9, 'active'=>1])->orderby('id', 'desc')->get();
-        $coverage_items = RootItem::where(['category'=>31, 'active'=>1])->orderby('id', 'desc')->get();
-        $client_items = RootItem::where(['category'=>51, 'active'=>1])->orderby('id', 'desc')->get();
+        $cooperation_items = RootItem::where(['category'=>9, 'active'=>1])->orderby('updated_at', 'desc')->get();
+        $coverage_items = RootItem::where(['category'=>41, 'active'=>1])->orderby('updated_at', 'desc')->get();
+        $client_items = RootItem::where(['category'=>51, 'active'=>1])->orderby('updated_at', 'desc')->get();
 
         $html = view('frontend.entrance.root')->with([
                 'rent_items'=>$rent_items,
