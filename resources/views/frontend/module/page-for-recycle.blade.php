7941e7f187e86f47cd4fbf59de586edd9cbae492
longyun <longyun@longyunMacBook-Pro.local>
2019-01-15 17:51:54 +0800

    recycle

--- resources/views/frontend/module/page-for-recycle.blade.php
+++ resources/views/frontend/module/page-for-recycle.blade.php
@@ -1,30 +1,46 @@
 {{--page-detail--}}{{--main--}}
 <section class="module-container bg-light">
-    <div class="container">
+    <div class="container main-container">
 
-        <header class="module-row module-header-container">
-            <div class="wow slideInLeft module-title-row color-1 border-light"><b>钢琴回收</b></div>
-            <div class="wow slideInRight module-subtitle-row color-6"><b>上海地区最实惠的租琴</b></div>
+        <header class="module-row module-header-container text-center">
+            <div class="wow slideInLeft module-title-row title-with-double-line color-1 border-light title-h2">钢琴回收</div>
+            <div class="wow slideInRight module-subtitle-row color-5 title-h4">深耕本地市场十余年，服务靠谱，价格靠谱</div>
         </header>
 
         <div class="module-row module-body-container">
             {{--图文详情--}}
             <section class="property-contents common">
                 <div class="entry-title clearfix">
-                    <h3 class="text-center"> {{ $data->title or '' }} </h3>
+                    <h4 class="text-left"> 雅马哈型号 </h4>
                 </div>
                 <div>
-                    <p>
-                        <img src="http://m.yundipiano.com/img/goods/c833d4a8-ddf8-4701-9727-1443fcf62337.jpg" alt=""/>
-                        <img src="http://m.yundipiano.com/img/goods/b10d2717-83ca-4fcd-89c5-68563c74f291.jpg" alt="Beifallssturm 贝芬思顿 BA121 立式钢琴"/>
-                        <img src="http://m.yundipiano.com/img/goods/0997c5f6-81f0-4613-96cc-08fd082c9ea2.jpg" alt="Beifallssturm 贝芬思顿 BA121 立式钢琴"/>
-                        <img src="http://m.yundipiano.com/img/goods/588f0776-9d43-4999-b61a-44e2a3e5e782.jpg" alt="Beifallssturm 贝芬思顿 BA121 立式钢琴"/>
-                        <img src="http://m.yundipiano.com/img/restsImg/7c599445-54f6-4265-8078-6b90f1334243.jpg" alt="Beifallssturm 贝芬思顿 BA121 立式钢琴"/>
-                        <img src="http://m.yundipiano.com/img/restsImg/f6039e3a-f072-4d1a-a678-9bdc2550e22d.jpg" alt="Beifallssturm 贝芬思顿 BA121 立式钢琴"/>
-                        <img src="http://m.yundipiano.com/img/restsImg/17c9c493-c463-440e-a315-145514748ec7.jpg" alt="Beifallssturm 贝芬思顿 BA121 立式钢琴"/>
-                        <img src="http://m.yundipiano.com/img/restsImg/1f75e8c4-bf39-4e98-b67c-d8f4971a45e8.jpg" alt="Beifallssturm 贝芬思顿 BA121 立式钢琴"/>
-                        <img src="http://m.yundipiano.com/img/restsImg/2dc83775-418c-4d82-88cd-d08fa72eefb0.jpg" alt="Beifallssturm 贝芬思顿 BA121 立式钢琴"/>
-                    </p>
+                    <p>常见U系列之U1：U1a, U1D, U1E, U1F, U1G, U1H, U1M, U1A, U10BL, U10A, U100, UX-1, UX10BL, UX10A</p>
+                    <p>常见U系列之U2：U2a, U2B, U2C, U2F, U2G, U2H, U2M, U2A, UX-2</p>
+                    <p>常见U系列之U3：U3a, U3C, U3E, U3F, U3G, U3H, U3M, U3A, U30BL, U30A, U300, UX-3, UX30BL, UX30A</p>
+                    <p>常见U系列之U5：U5B, U5D, U5H</p>
+                    <p>常见U系列之U7：U7B, U7C, U7H</p>
+                </div>
+                <div>
+                    <br>
+                </div>
+                <div class="text-center">
+                    <div><img src="{{ asset('/custom/images/common/1.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/2.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/3.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/4.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/5.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/6.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/7.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/8.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/9.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/10.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/11.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/12.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/13.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/14.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/15.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/16.jpg') }}" alt="Property Image"></div>
+                    <div><img src="{{ asset('/custom/images/common/17.jpg') }}" alt="Property Image"></div>
                 </div>
             </section>
         </div>
