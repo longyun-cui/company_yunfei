83bc8d22d2ca2b07a1e1829942a475aa9b44c26e
Lonyun Cui <longyun-cui@163.com>
2019-08-13 17:03:17 +0800

    color

--- resources/views/frontend/module/root-rent-out.blade.php
+++ resources/views/frontend/module/root-rent-out.blade.php
@@ -9,7 +9,7 @@
 
         <div class="module-row module-body-container">
             @foreach($items as $v)
-                <div class="item-col col-lg-3 col-md-4 col-sm-6 col-xs-6">
+                <div class="item-col col-lg-3 col-md-4 col-sm-6 col-xs-6" style="padding:8px;">
                     <div class="item-container">
 
                         <figure class="image-container padding-top-3-4">
