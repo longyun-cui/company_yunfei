3a0bb1fd460c4221bd36be5678bce2424df0cb74
Lonyun Cui <longyun-cui@163.com>
2019-12-31 16:24:06 +0800

    root

--- resources/views/frontend/module/list-for-rent-out.blade.php
+++ resources/views/frontend/module/list-for-rent-out.blade.php
@@ -9,13 +9,13 @@
 
         <div class="module-row module-body-container">
             @foreach($items as $v)
-                <div class="col-lg-3 col-md-4 col-sm-6 item-col">
-                    <div class="item-container">
+                <div class="col-lg-3 col-md-4 col-sm-6 item-col" style="padding:8px;">
+                    <div class="item-container padding-8px">
 
                         <figure class="image-container padding-top-3-4">
                             <div class="image-box">
                                 <a class="clearfix zoom-" target="_blank"  href="{{ url('/item/'.$v->id) }}">
-                                    <img data-action="zoom-" src="{{ config('common.host.'.env('APP_ENV').'.cdn').'/'.$v->cover_pic }}" alt="Property Image">
+                                    <img data-action="zoom-" src="{{ url(env('DOMAIN_CDN').'/'.$v->cover_pic) }}" alt="Property Image">
                                 </a>
                                 {{--<span class="btn btn-warning">热销中</span>--}}
                             </div>
@@ -25,17 +25,17 @@
                             <div class="text-box">
                                 <div class="text-title-row multi-ellipsis-2"><a href="{{ url('/item/'.$v->id) }}"><b>{{ $v->title or '' }}</b></a></div>
                                 <div class="text-description-row">
-                                    <div>
-                                        <i class="fa fa-cny"></i> <span class="font-18px color-red"><b>{{ $v->custom->price or '' }}</b></span>
+                                    <div class="font-14px">
+                                        <span>租金：<i class="fa fa-cny"></i> <b class="color-red">{{ $v->custom->price or '' }}</b></span>
                                     </div>
-                                    <div>
+                                    <div class="font-14px">
                                         <span>押金：<i class="fa fa-cny"></i> {{ $v->custom->deposit or '' }} </span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="text-box with-border-top clearfix">
+                            <div class="text-box with-border-top text-center clearfix">
                                 <a target="_blank" href="{{ url('/item/'.$v->id) }}">
-                                    <button class="btn btn-default btn-price pull-right btn-3d" data-hover="点击查看" style="border-radius:0;">
+                                    <button class="btn btn-default btn-price pull-right- btn-3d" data-hover="点击查看" style="border-radius:0;">
                                         <strong>查看详情</strong>
                                     </button>
                                 </a>
--- resources/views/frontend/module/root-video.blade.php
+++ resources/views/frontend/module/root-video.blade.php
@@ -17,20 +17,20 @@
                     </a>
                     <a class="btn btn-ghost btn-ghost-white btn-lg btn-block lightcase-video- _none" data-rel="lightcase" role="button"
                        href="" data-lc-options='{width:640, height:336, autoplay:false}'
-                       data-lc-href="{{ config('common.host.'.env('APP_ENV').'.cdn').trans('custom.text.video') }}">
+                       data-lc-href="http://cdn.keron-relo.com/videos/KERON_ZH.mp4">
                         <i class="fa fa-play-circle-o" style="font-size:64px;"></i>
                     </a>
 
 
                     <a class="btn btn-ghost btn-ghost-white btn-lg btn-block _none" data-fancybox- data-width="640" data-height="336"
-                       href="{{ config('common.host.'.env('APP_ENV').'.cdn').trans('custom.text.video') }}">
+                       href="http://cdn.keron-relo.com/videos/KERON_ZH.mp4">
                         <i class="fa fa-play-circle-o" style="font-size:64px;"></i>
                     </a>
                     <a class="btn btn-ghost btn-ghost-white btn-lg btn-block" data-fancybox="" href="#myVideo">
                         <i class="fa fa-play-circle-o" style="font-size:64px;"></i>
                     </a>
                     <video width="720" height="416" controls="" id="myVideo" style="display:none;">
-                        <source src="{{ config('common.host.'.env('APP_ENV').'.cdn').trans('custom.text.video') }}" type="video/mp4">
+                        <source src="http://cdn.keron-relo.com/videos/KERON_ZH.mp4" type="video/mp4">
                     </video>
 
                 </p>
@@ -44,6 +44,6 @@
     </div>
 </section>
 <style>
-    #module-video { background:url(/custom/images/banner8.jpeg); background-size:cover; }
+    #module-video { background:url(/common/images/bg-11.jpg); background-size:cover; }
 </style>
 {{--<!-- END: module-link-contact -->--}}
