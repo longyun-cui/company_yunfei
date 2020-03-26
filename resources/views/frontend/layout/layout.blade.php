91f2ebdfb559e06bcbadfe9faa341c15259457d8
Lonyun Cui <longyun-cui@163.com>
2020-03-26 15:37:31 +0800

    root-course

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -42,11 +42,11 @@
 
         {{--<link href="{{ asset('/common/css/common.css') }}" rel="stylesheet">--}}
         <link href="{{ asset('http://cui.party/common/css/common.css') }}" rel="stylesheet">
-        <link href="{{ asset('http://gps.com/common/css/common.css') }}" rel="stylesheet">
+        {{--<link href="{{ asset('http://gps.com/common/css/common.css') }}" rel="stylesheet">--}}
 
         {{--<link href="{{ asset('/common/css/frontend.css') }}" rel="stylesheet">--}}
         <link href="{{ asset('http://cui.party/common/css/frontend.css') }}" rel="stylesheet">
-        <link href="{{ asset('http://gps.com/common/css/frontend.css') }}" rel="stylesheet">
+        {{--<link href="{{ asset('http://gps.com/common/css/frontend.css') }}" rel="stylesheet">--}}
 
         <link href="{{ asset('/common/css/animate/hover.css') }}" rel="stylesheet" media="all" />
         <link href="{{ asset('/common/css/animate/wicked.css') }}" rel="stylesheet" media="all" />
