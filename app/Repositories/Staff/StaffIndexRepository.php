651eb6520cb7003f8fe27475dda8a3c893389558
Lonyun Cui <longyun-cui@163.com>
2021-11-18 23:29:56 +0800

    debug

--- app/Repositories/Staff/StaffIndexRepository.php
+++ app/Repositories/Staff/StaffIndexRepository.php
@@ -410,8 +410,9 @@ class StaffIndexRepository {
                 {
                     if($operate == 'create')
                     {
+
                         copy(storage_path("resource/unique/portrait/user0.jpeg"), storage_path("resource/staff/unique/portrait/user_".$mine->id.".jpeg"));
-                        $mine->portrait_img = "staff/unique/portrait/user_".$mine->id.".jpg";
+                        $mine->portrait_img = "staff/unique/portrait/user_".$mine->id.".jpeg";
                         $mine->save();
                     }
                 }
